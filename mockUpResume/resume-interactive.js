32fad8005d9d308d001ae049c778b5073055e07b
monica-suarez <shutterbugmo@gmail.com>
2020-03-24 16:27:33 -0500

    name button works

--- mockUpResume/resume-interactive.js
+++ mockUpResume/resume-interactive.js
@@ -6,3 +6,13 @@ const showMobile = () => {
       x.style.display = "none";
     }
   }
+  
+  const fadeIn = () => {
+      let x = document.getElementById("peter");
+      if (x.style.display === "none" || x.style.display == '') {
+        x.style.display = "block";
+      } else {
+        x.style.display = "none";
+      }
+
+  }
